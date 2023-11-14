2043dbaa7b15b403d38c2baf5909b40f2fdaa882
PaulaPinto001 <83008038+PaulaPinto001@users.noreply.github.com>
2023-11-14 15:41:14 +0100

    Update wallet.py bug on purpose

--- wallet.py
+++ wallet.py
@@ -3,10 +3,10 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
-        self.balance = self.balance + val
+        selfo.balance = self.balance + val
 
     def get_balance(self):
-        return self.balance
+        return self.balhance
 
     def remove_balance(self, val):
-        self.balance = self.balance - val
+        self.balance = self.balance - vagl
